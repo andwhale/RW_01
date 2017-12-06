3f8864b73732437580ed2d183a61979d9fe67b4e
Andrew Velikiy <andwhale@gmail.com>
2017-12-06 20:23:46 +0300

    Added About Author VC.

---
+++ RW_01/AboutAuthorVC.swift
@@ -0,0 +1,16 @@
+//
+//  AboutAuthorVC.swift
+//  RW_01
+//
+//  Created by Andrey Velikiy on 12/6/17.
+//  Copyright © 2017 Andrey Velikiy. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorVC: UIViewController {
+    
+    @IBAction func touch () {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ RW_01/InfoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  InfoViewController.swift
+//  RW_01
+//
+//  Created by Andrey Velikiy on 12/6/17.
+//  Copyright © 2017 Andrey Velikiy. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+//    @IBAction func aboutAuthor () {
+//
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close () {
+        dismiss(animated: true, completion: nil)
+    }
+}
