d2f091bd17e22b35483482fae08566254b5710e5
Andrew Velikiy <andwhale@gmail.com>
2017-12-07 19:33:11 +0300

    Added WKWebView to load Instructions from local html Added Activity Indicator

--- RW_01/InfoViewController.swift
+++ RW_01/InfoViewController.swift
@@ -7,17 +7,35 @@
 //
 
 import UIKit
+import WebKit
 
-class InfoViewController: UIViewController {
+class InfoViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
     
-//    @IBAction func aboutAuthor () {
-//
-//    }
-    
+	@IBOutlet weak var instructions: WKWebView!
+	var activityIndicator: UIActivityIndicatorView!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
+		//load HTML file
+		let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html")
+		instructions.loadFileURL(url!, allowingReadAccessTo: url!)
+		instructions.navigationDelegate = self
+		//add/start activity indicator
+		activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+		activityIndicator.center = view.center
+		activityIndicator.startAnimating()
+		self.view.addSubview(activityIndicator)
+		activityIndicator.hidesWhenStopped = true
     }
-    
+	
+	//delegate method to hide activity indicator on page load
+	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+		if !instructions.isLoading {
+			activityIndicator.stopAnimating()
+			activityIndicator.isHidden = true
+		}
+	}
+	
     @IBAction func close () {
         dismiss(animated: true, completion: nil)
     }
